3cf3e76e8b907eacd8f3ca58a4d5b065b9297623
Sunzheini <daniel_zorov@abv.bg>
2022-08-30 17:49:00 +0300

    Update L2_Searching_and_Sorting_Algorithms.py

--- lab/L2_Searching_and_Sorting_Algorithms.py
+++ lab/L2_Searching_and_Sorting_Algorithms.py
@@ -1,2 +1,24 @@
 # Searching and Sorting Algorithms
 
+# -----------------------------------------------------------------------------------------------
+# Linear Search - check elements one by one, i.e. for an element in a list
+
+nums = [1, 2, 3, 4, 5, 6, 7]
+
+target = 5
+for idx, i in enumerate(nums):
+    if i == target:
+        print(f"Index of target is {idx}")
+        break
+
+
+# -----------------------------------------------------------------------------------------------
+# Binary Search
+
+
+
+
+
+
+
+
