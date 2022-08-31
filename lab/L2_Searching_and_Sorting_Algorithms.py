8ecd204516f6f78093bdf326b96df1be4d10e405
Sunzheini <daniel_zorov@abv.bg>
2022-08-31 09:57:22 +0300

    Update L2_Searching_and_Sorting_Algorithms.py

--- lab/L2_Searching_and_Sorting_Algorithms.py
+++ lab/L2_Searching_and_Sorting_Algorithms.py
@@ -3,18 +3,30 @@
 # -----------------------------------------------------------------------------------------------
 # Linear Search - check elements one by one, i.e. for an element in a list
 
-nums = [1, 2, 3, 4, 5, 6, 7]
-
-target = 5
-for idx, i in enumerate(nums):
-    if i == target:
-        print(f"Index of target is {idx}")
-        break
+# nums = [1, 2, 3, 4, 5, 6, 7]
+#
+# target = 5
+# for idx, i in enumerate(nums):
+#     if i == target:
+#         print(f"Index of target is {idx}")
+#         break
 
 
 # -----------------------------------------------------------------------------------------------
-# Binary Search
+# Binary Search - spisykyt v koito tyrsim trqbva da e predvariteln o podreden
+                # sled tova sravnqvame sys sredniq element
+                # postoqnno svivame masiva na 2
 
+nums = [1, 2, 3, 4, 5, 6, 7]
+target = 5
+
+
+
+
+
+
+
+# -----------------------------------------------------------------------------------------------
 
 
 
