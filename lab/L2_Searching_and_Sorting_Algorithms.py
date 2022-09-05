089ac800314640c79cffc2c214b70218ff7a30dc
Sunzheini <daniel_zorov@abv.bg>
2022-09-05 16:39:48 +0300

    Update05092022

--- lab/L2_Searching_and_Sorting_Algorithms.py
+++ lab/L2_Searching_and_Sorting_Algorithms.py
@@ -17,18 +17,33 @@
                 # sled tova sravnqvame sys sredniq element
                 # postoqnno svivame masiva na 2
 
-nums = [1, 2, 3, 4, 5, 6, 7]
-target = 5
-
-
-
-
-
-
+# nums = [1, 2, 3, 4, 5, 6, 7]
+# target = 5
 
 # -----------------------------------------------------------------------------------------------
 
+# 1
+def binary_search(nums, target):
+    left = 0
+    right = len(nums) - 1
 
+    while left <= right:
+        mid_idx = (left + right) // 2
+        mid_el = nums[mid_idx]
+
+        if mid_el == target:
+            return mid_idx
+
+        if target > mid_el:
+            left = mid_idx + 1
+        else:
+            right = mid_idx - 1
+
+
+nums = [int(x) for x in input().split()]
+target = int(input())
+
+print(binary_search(nums, target))
 
 
 
---
+++ lab/L3_Introduction_to_Graphs.py
@@ -0,0 +1,7 @@
+# Introduction to Graphs
+
+
+# graph e vid struktura
+# G(V,E) (node-ove, rebra)
+
+
