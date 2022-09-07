0f993d2e64b23f4fb1e56073d89eea958365a706
Sunzheini <daniel_zorov@abv.bg>
2022-09-07 17:34:17 +0300

    Update exam_prep_1.py

--- lab/exam_prep_1.py
+++ lab/exam_prep_1.py
@@ -1,7 +1,44 @@
-# 01. The story telling
+# 01. The story telling (Graph using dfs)
+from collections import deque
 
 
+def dfs(node, graph, visited):
+    if node in visited:
+        return
+    visited.add(node)
+    for child in graph[node]:
+        dfs(child, graph, visited)
+    result.appendleft(node)
 
 
+graph = {}
+result = deque()
+while 1:
+    line = input()
+    if line == 'End':
+        break
 
+    node, children_str = [x.strip() for x in line.split('->')]
+    children = children_str.split()
+    graph[node] = children
+
+visited = set()
+
+for node in graph:
+    dfs(node, graph, visited)
+
+print(' '.join(result))
+
+
+# Task Input:
+# Mort -> Time Space
+# Time -> Future Robot
+# Space -> Metro
+# Future -> Space Metro
+# Robot -> Future
+# Metro ->
+# End
+
+# -----------------------------------------------------------------------------
+# -1.20
 
