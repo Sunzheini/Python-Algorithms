72d6d6a050feb39a4939d7612b9dbfda9ba6290a
Sunzheini <daniel_zorov@abv.bg>
2022-09-08 16:40:15 +0300

    Update08092022-1

--- lab/exam_prep_1.py
+++ lab/exam_prep_1.py
@@ -1,33 +1,33 @@
 # 01. The story telling (Graph using dfs)
-from collections import deque
+# from collections import deque
 
 
-def dfs(node, graph, visited):
-    if node in visited:
-        return
-    visited.add(node)
-    for child in graph[node]:
-        dfs(child, graph, visited)
-    result.appendleft(node)
+# def dfs(node, graph, visited):
+#     if node in visited:
+#         return
+#     visited.add(node)
+#     for child in graph[node]:
+#         dfs(child, graph, visited)
+#     result.appendleft(node)
 
 
-graph = {}
-result = deque()
-while 1:
-    line = input()
-    if line == 'End':
-        break
+# graph = {}
+# result = deque()
+# while 1:
+#     line = input()
+#     if line == 'End':
+#         break
 
-    node, children_str = [x.strip() for x in line.split('->')]
-    children = children_str.split()
-    graph[node] = children
+#     node, children_str = [x.strip() for x in line.split('->')]
+#     children = children_str.split()
+#     graph[node] = children
 
-visited = set()
+# visited = set()
 
-for node in graph:
-    dfs(node, graph, visited)
+# for node in graph:
+#     dfs(node, graph, visited)
 
-print(' '.join(result))
+# print(' '.join(result))
 
 
 # Task Input:
@@ -40,5 +40,20 @@ print(' '.join(result))
 # End
 
 # -----------------------------------------------------------------------------
-# -1.20
+# 2
+
+
+
+
+
+
+# Task Input:
+# 13 42 69 73 42 84 26
+# 13 54 73 42 8 15 29
+
+# -----------------------------------------------------------------------------
+# 3
+
+
+
 
